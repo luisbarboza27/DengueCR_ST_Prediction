bc86b3e07d9164c0ad1000657f69455124042f2f
Luis Barboza <luis@localhost.localdomain>
2021-05-18 21:47:07 -0600

    Agrega ajuste GAMLSS

--- 2.run.GAM.Temporal.PureProy.R
+++ 2.run.GAM.Temporal.PureProy.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(timetk)
 library(lubridate)
+library(gamlss)
 
 # Carga de datos con riesgos relativos, índices climáticos y variables
 # climáticas de MODIS
@@ -12,11 +13,11 @@ maxlag <- 24
 
 
 cantones <- datos_totales %>%
-  select(CCanton) %>% distinct()
+  dplyr::select(CCanton) %>% distinct()
 
 ccf_dataset <- datos_totales %>%
   mutate(Date=make_date(Year,Month,1)) %>%
-  select(Date,Canton,RR,Nino12SSTA,Nino3SSTA,Nino34SSTA,Nino4SSTA,EVI,NDVI,
+  dplyr::select(Date,Canton,RR,Nino12SSTA,Nino3SSTA,Nino34SSTA,Nino4SSTA,EVI,NDVI,
        NDWI,LSD,LSN,TNA,Precip_t) %>%
   group_by(Canton) %>%
   tk_acf_diagnostics(Date,RR,
@@ -26,26 +27,29 @@ ccf_dataset <- datos_totales %>%
 
 lags_ccf <- ccf_dataset %>% group_by(Canton) %>%
   filter(lag!=0) %>% 
-  mutate(lagSSTA12 = which.max(abs(CCF_Nino12SSTA)),
-         lagSSTA3 = which.max(abs(CCF_Nino3SSTA)),
-         lagSSTA34 = which.max(abs(CCF_Nino34SSTA)),
-         lagSSTA4 = which.max(abs(CCF_Nino4SSTA)),
-         lagEVI = which.max(abs(CCF_EVI)),
-         lagNDVI = which.max(abs(CCF_NDVI)),
-         lagNDWI = which.max(abs(CCF_NDWI)),
-         lagLSD = which.max(abs(CCF_LSD)),
-         lagLSN = which.max(abs(CCF_LSN)),
-         lagTNA = which.max(abs(CCF_TNA)),
-         lagPrec = which.max(abs(CCF_Precip_t))) %>%
-  select(Canton,starts_with('lag'),-lag) %>%
+  mutate(lagSSTA12 = max(which.max(abs(CCF_Nino12SSTA)),3),
+         lagSSTA3 = max(which.max(abs(CCF_Nino3SSTA)),3),
+         lagSSTA34 = max(which.max(abs(CCF_Nino34SSTA)),3),
+         lagSSTA4 = max(which.max(abs(CCF_Nino4SSTA)),3),
+         lagEVI = max(which.max(abs(CCF_EVI)),3),
+         lagNDVI = max(which.max(abs(CCF_NDVI)),3),
+         lagNDWI = max(which.max(abs(CCF_NDWI)),3),
+         lagLSD = max(which.max(abs(CCF_LSD)),3),
+         lagLSN = max(which.max(abs(CCF_LSN)),3),
+         lagTNA = max(which.max(abs(CCF_TNA)),3),
+         lagPrec = max(which.max(abs(CCF_Precip_t))),3) %>%
+  dplyr::select(Canton,starts_with('lag'),-lag) %>%
   distinct()
-  
+
+
 #write.csv(lags_ccf,'lags_dengue.csv')
 
 bases_ent <- list() # Bases de entrenamiento por canton
 bases_test <- list() # Bases de prueba por canton
 
 lags_ccf <- as.matrix(lags_ccf[,-1])
+valores_p <- NULL
+AA <- NULL
 
 for(i in 1:length(cantones$CCanton)){
   show(paste0('Ajuste y Predicción-Canton-',i))
@@ -70,6 +74,18 @@ for(i in 1:length(cantones$CCanton)){
     drop_na(Nino12SSTAl1,Nino3SSTAl1,Nino34SSTAl1,Nino4SSTAl1,EVIl1,NDVIl1,
             NDWIl1,LSDl1,LSNl1,TNAl1,Precipl1) 
   
+  
+  modelo_gamma <- gamlss(RR~Nino3SSTAl1+Precipl1+as.factor(Month),
+                          data = base_ent,
+                          family = ZAGA())
+  modelo_normal <- gamlss(RR~Nino3SSTAl1+Precipl1+as.factor(Month),
+                          data = base_ent)
+  modelo_IG <- gamlss(RR~Nino3SSTAl1+Precipl1+as.factor(Month),
+                          data = base_ent,
+                          family = ZAIG())
+  AA <- rbind(AA,t(AIC(modelo_gamma,modelo_normal,modelo_IG))[2,])
+  uu <- drop1(modelo_gamma)
+  valores_p <- rbind(valores_p,uu$`Pr(Chi)`)
   bases_ent[[i]] <- base_ent
   bases_test[[i]] <- base_test
 }
