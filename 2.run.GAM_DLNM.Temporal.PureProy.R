972190b71a7d5887cc9ce9eec6f996a77c76b04e
Luis Barboza <lbarbo@gmail.com>
2021-08-24 21:29:17 -0600

    Agrega cambios 11/08

--- 2.run.GAM_DLNM.Temporal.PureProy.R
+++ 2.run.GAM_DLNM.Temporal.PureProy.R
@@ -13,45 +13,16 @@ load('./Data/datos_totales.RData')
 
 ##ESCOGENCIA DE REZAGO MÁXIMO ####
 maxlag <- 18
-
+minlag <- 3
 
 cantones <- datos_totales %>%
   dplyr::select(CCanton) %>% distinct()
-
-ccf_dataset <- datos_totales %>%
-  mutate(Date=make_date(Year,Month,1)) %>%
-  dplyr::select(Date,Canton,RR,Nino12SSTA,Nino3SSTA,Nino34SSTA,Nino4SSTA,EVI,NDVI,
-                NDWI,LSD,LSN,TNA,Precip_t) %>%
-  group_by(Canton) %>%
-  tk_acf_diagnostics(Date,RR,
-                     .ccf_vars=c(Nino12SSTA,Nino3SSTA,Nino34SSTA,Nino4SSTA,EVI,NDVI,
-                                 NDWI,LSD,LSN,TNA,Precip_t),
-                     .lags=0:maxlag)
-
-lags_ccf <- ccf_dataset %>% group_by(Canton) %>%
-  filter(lag!=0) %>% 
-  mutate(lagSSTA12 = max(which.max(abs(CCF_Nino12SSTA)),3),
-         lagSSTA3 = max(which.max(abs(CCF_Nino3SSTA)),3),
-         lagSSTA34 = max(which.max(abs(CCF_Nino34SSTA)),3),
-         lagSSTA4 = max(which.max(abs(CCF_Nino4SSTA)),3),
-         lagEVI = max(which.max(abs(CCF_EVI)),3),
-         lagNDVI = max(which.max(abs(CCF_NDVI)),3),
-         lagNDWI = max(which.max(abs(CCF_NDWI)),3),
-         lagLSD = max(which.max(abs(CCF_LSD)),3),
-         lagLSN = max(which.max(abs(CCF_LSN)),3),
-         lagTNA = max(which.max(abs(CCF_TNA)),3),
-         lagPrec = max(which.max(abs(CCF_Precip_t))),3) %>%
-  dplyr::select(Canton,starts_with('lag'),-lag) %>%
-  distinct()
-
-
-#write.csv(lags_ccf,'lags_dengue.csv')
 
 bases_ent <- list() # Bases de entrenamiento por canton
 bases_test_1 <- list() # Bases de prueba por canton
 bases_test_2 <- list() # Bases de prueba por canton
 
-lags_ccf <- as.matrix(lags_ccf[,-1])
+
 valores_p <- NULL
 AA <- NULL
 
@@ -71,34 +42,67 @@ for(i in 1:length(cantones$CCanton)){
   
   
   ##########
+  basecanton <- basecanton %>% mutate(RRl1 = lag(RR,3),
+                                      Precip_t_3 = lag(Precip_t,3),
+                                      Nino3SSTA_3 = lag(Nino3SSTA,3),
+                                      TNA_3 = lag(TNA,3)) 
   basecanton <- basecanton %>% drop_na()
   #basis.P <- crossbasis(basecanton$Precip_t,lag = 12,argvar=list(fun="lin"),
   #                      arglag=list(fun="poly",degree=2))
-  basis.P <- crossbasis(basecanton$Precip_t,
+  basis.P <- crossbasis(basecanton$Precip_t_3,
                         argvar = list(fun='lin'),
                         arglag = list(fun='lin'),
                         lag=18)
-  basecanton <- basecanton %>% bind_cols(data.frame(basis.P)) %>%
+  colnames(basis.P) <- paste0('P',1:dim(basis.P)[2])
+  basis.Nino <- crossbasis(basecanton$Nino3SSTA_3,
+                        argvar = list(fun='lin'),
+                        arglag = list(fun='lin'),
+                        lag=18)
+  colnames(basis.Nino) <- paste0('N',1:dim(basis.Nino)[2])
+  basis.TNA <- crossbasis(basecanton$TNA_3,
+                           argvar = list(fun='lin'),
+                           arglag = list(fun='lin'),
+                           lag=18)
+  colnames(basis.TNA) <- paste0('T',1:dim(basis.TNA)[2])
+  
+  basis.RR <- crossbasis(basecanton$RRl1,
+                          argvar = list(fun='lin'),
+                          arglag = list(fun='lin'),
+                          lag=11)
+  colnames(basis.RR) <- paste0('R',1:dim(basis.TNA)[2])
+  
+  
+  basecanton <- basecanton %>% 
+    bind_cols(data.frame(basis.P),
+              data.frame(basis.Nino),
+              data.frame(basis.TNA),
+              data.frame(basis.RR)) %>%
     drop_na()
-  modelo_gamma <- gamlss(RR~v1.l1+v1.l2,
+  
+
+  base_ent <- basecanton %>% slice(1:(n()-15)) 
+  
+  modelo_gamma <- gamlss(RR~R1+R2+
+                           P1+P2+
+                           N1+N2+
+                           T1+T2,
                          sigma.formula = ~1,
                          nu.formula = ~1,
-                         data = basecanton,
+                         data = base_ent,
                          family = ZAGA(),
                          control = gamlss.control(trace = F))
   
   ##########
-  basecanton <- basecanton %>%
-    mutate(RRl1 = lag(RR),Nino12SSTAl1=lag(Nino12SSTA,lags_ccf[i,1]),Nino3SSTAl1=lag(Nino3SSTA,lags_ccf[i,2]),Nino34SSTAl1=lag(Nino34SSTA,lags_ccf[i,3]),Nino4SSTAl1=lag(Nino4SSTA,lags_ccf[i,4]),
-           EVIl1=lag(EVI,lags_ccf[i,5]),NDVIl1=lag(NDVI,lags_ccf[i,6]),
-           NDWIl1=lag(NDWI,lags_ccf[i,7]),LSDl1=lag(LSD,lags_ccf[i,8]),
-           LSNl1=lag(LSN,lags_ccf[i,9]),TNAl1=lag(TNA,lags_ccf[i,10]),
-           Precipl1 = lag(Precip_t, lags_ccf[i,11])) %>%
-    dplyr::select(Year,Month,RR,RRl1,Nino12SSTAl1,Nino3SSTAl1,Precipl1,TNAl1)
+  # basecanton <- basecanton %>%
+  #   mutate(RRl1 = lag(RR),Nino12SSTAl1=lag(Nino12SSTA,lags_ccf[i,1]),Nino3SSTAl1=lag(Nino3SSTA,lags_ccf[i,2]),Nino34SSTAl1=lag(Nino34SSTA,lags_ccf[i,3]),Nino4SSTAl1=lag(Nino4SSTA,lags_ccf[i,4]),
+  #          EVIl1=lag(EVI,lags_ccf[i,5]),NDVIl1=lag(NDVI,lags_ccf[i,6]),
+  #          NDWIl1=lag(NDWI,lags_ccf[i,7]),LSDl1=lag(LSD,lags_ccf[i,8]),
+  #          LSNl1=lag(LSN,lags_ccf[i,9]),TNAl1=lag(TNA,lags_ccf[i,10]),
+  #          Precipl1 = lag(Precip_t, lags_ccf[i,11])) %>%
+  #   dplyr::select(Year,Month,RR,RRl1,Nino12SSTAl1,Nino3SSTAl1,Precipl1,TNAl1)
   
   
-  base_ent <- basecanton %>% slice(1:(n()-15)) %>%
-    drop_na(RRl1,Nino12SSTAl1,Nino3SSTAl1,Precipl1,TNAl1)
+
   #    drop_na(Nino12SSTAl1,Nino3SSTAl1,Nino34SSTAl1,Nino4SSTAl1,EVIl1,NDVIl1,
   #            NDWIl1,LSDl1,LSNl1,TNAl1,Precipl1)
   
@@ -107,12 +111,12 @@ for(i in 1:length(cantones$CCanton)){
   #            NDWIl1,LSDl1,LSNl1,TNAl1,Precipl1) 
   
   base_test_2 <- basecanton %>%  slice((n()-2):n()) 
-  modelo_gamma <- gamlss(RR~RRl1+Nino3SSTAl1+TNAl1+Precipl1+Month,
-                         sigma.formula = ~1,
-                         nu.formula = ~1,
-                         data = base_ent,
-                         family = ZAGA(),
-                         control = gamlss.control(trace = F))
+  # modelo_gamma <- gamlss(RR~RRl1+Nino3SSTAl1+TNAl1+Precipl1+Month,
+  #                        sigma.formula = ~1,
+  #                        nu.formula = ~1,
+  #                        data = base_ent,
+  #                        family = ZAGA(),
+  #                        control = gamlss.control(trace = F))
   
   
   base_ent <- base_ent %>% 
